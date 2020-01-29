0ec296488d2ea8489f879105b59452bc7d9e8f48
Lillian <59583025+lragudo@users.noreply.github.com>
2020-01-29 14:02:44 -0800

    Proof of concept

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('OK');
+// console.log('OK');
+
+const player = new Plyr("#player", {
+    controls: [ 'captions'],
+    keyboard: {focused: true, global: true},
+    ads: { enabled: false, publisherId: '' }
+    
+  });
+  console.log(player);  
+  
